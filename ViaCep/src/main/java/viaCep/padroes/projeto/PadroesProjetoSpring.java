6772ee0afae35fbca8c55f28585163bccd997714
Larissa <larissa.rfs28@gmail.com>
2024-09-30 11:11:01 -0300

    Adicionando novo arquivo.

---
+++ ViaCep/src/main/java/viaCep/padroes/projeto/PadroesProjetoSpring.java
@@ -0,0 +1,15 @@
+package dio.padroes.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PadroesProjetoSpring {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjetoSpring.class, args);
+	}
+
+}
