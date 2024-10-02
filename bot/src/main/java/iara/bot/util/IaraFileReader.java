db6cfb1003d7d0ee73e490dbd7d7229c91dc3918
Savio <saviomariano77@gmail.com>
2024-10-02 10:20:23 -0300

    adicionando retorno das mensagens do usuario

--- bot/src/main/java/iara/bot/util/IaraFileReader.java
+++ bot/src/main/java/iara/bot/util/IaraFileReader.java
@@ -16,18 +16,21 @@ public class IaraFileReader {
         this.FILE_PATH = FILE_PATH;
     }
 
-    public void readerIara(){
+    public String readerIara(){
         try{
         File file = new File(FILE_PATH);
         Scanner reader = new Scanner(file);
         while(reader.hasNextLine()){
             String linha = reader.nextLine();
+            return linha;
         }
         reader.close();   
         }catch(FileNotFoundException e){
             System.out.println("Um erro ocorreu.");
             e.printStackTrace();
+            
         }
+        return "tudo correto";
     }
     
   
