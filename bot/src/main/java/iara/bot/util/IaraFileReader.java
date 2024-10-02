3380e31a3b3950c35698b545187e269ff1f56772
Savio <saviomariano77@gmail.com>
2024-10-02 20:56:49 -0300

    atualizando a logica de leitura

--- bot/src/main/java/iara/bot/util/IaraFileReader.java
+++ bot/src/main/java/iara/bot/util/IaraFileReader.java
@@ -4,30 +4,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IaraFileReader {
     
-    @Value("mensagens.txt")
+    @Value("C:\\Users\\melio\\Documents\\projeto-iara\\back-end-IARA\\bot\\src\\main\\java\\iara\\bot\\mensagens-chatbot\\mensagens.txt")
     private String FILE_PATH;
 
-    @Autowired
     public IaraFileReader(String FILE_PATH){
         this.FILE_PATH = FILE_PATH;
     }
 
-    public String readerIara(){
+    public String leitorIara(String entradaUsuario){
         try{
         File file = new File(FILE_PATH);
         Scanner reader = new Scanner(file);
         while(reader.hasNextLine()){
             String linha = reader.nextLine();
             return linha;
-        }
-        reader.close();   
+        }   
+        reader.close(); 
         }catch(FileNotFoundException e){
             System.out.println("Um erro ocorreu.");
             e.printStackTrace();
@@ -35,5 +33,4 @@ public class IaraFileReader {
         }
         return "tudo correto";
     }
-  
 }
