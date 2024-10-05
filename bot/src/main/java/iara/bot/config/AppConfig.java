36e940b7e1130b697b6da61e2a57e52325c3202f
Larissa <larissa.rfs28@gmai.com>
2024-10-05 20:28:38 -0300

    Criando novo arquivo.
    Co-authored-by: Savio <SavioMariano@users.noreply.github.com>

--- bot/src/main/java/iara/bot/config/AppConfig.java
+++ bot/src/main/java/iara/bot/config/AppConfig.java
@@ -3,12 +3,19 @@ package iara.bot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import iara.bot.model.MensagemModel;
 import iara.bot.util.IaraFileReader;
 
 @Configuration
 public class AppConfig {
     @Bean
     public IaraFileReader iaraFileReader(){
-        return new IaraFileReader("mensagens-chatbot/mensagens.txt");
+        return new IaraFileReader();
     }
+
+    @Bean
+    public MensagemModel mensagemModel(){
+        return new MensagemModel();
+    }
+
 }
